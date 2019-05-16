922d916c4113667dafb426b6ad4a5b2e00522d84
SriSINDU <srisindu.3@gmail.com>
2019-05-16 12:37:02 +0530

    inheritance concepts

--- src/oopsconcepts/TestSuperKeyword.java
+++ src/oopsconcepts/TestSuperKeyword.java
@@ -2,16 +2,32 @@ package oopsconcepts;
 class Vehicle1
 {
     int maxSpeed = 120;
+    Vehicle1(){
+        System.out.println("This is base class constructor");
+    }
+    void message()
+    {
+        System.out.println("This is Base class");
+    }
 }
 
 /* sub class Car extending vehicle */
 class Car extends Vehicle1
 {
     int maxSpeed = 180;
+    Car(){
+        super();
+
+    }
+    void message()
+    {
+        System.out.println("This is child class");
+    }
     void display()
     {
         /* print maxSpeed of base class (vehicle) */
         System.out.println("Maximum Speed: " + super.maxSpeed);
+        super.message();
     }
 }
 
