2274bacb61e06a631caca54f4d42666e32804837
SriSINDU <srisindu.3@gmail.com>
2019-05-16 14:07:28 +0530

    inheritance concepts

---
+++ src/oopsconcepts/IsInstanceOf.java
@@ -0,0 +1,10 @@
+package oopsconcepts;
+
+public class IsInstanceOf {
+
+        public static void main(String args[]){
+            IsInstanceOf d=null;
+            System.out.println(d instanceof IsInstanceOf);//false
+        }
+    }
+
