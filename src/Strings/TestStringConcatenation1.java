bb441d3844b60ebb78062c2750e240988aa9bbb3
SriSINDU <srisindu.3@gmail.com>
2019-05-17 13:53:35 +0530

    strings

---
+++ src/Strings/TestStringConcatenation1.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class TestStringConcatenation1 {
+    public static void main(String args[]){
+        String s="Sachin"+" Tendulkar";
+        System.out.println(s);//Sachin Tendulkar
+        String s1=50+30+"Sachin"+40+40;
+        System.out.println(s1);//80Sachin4040
+    }
+}
---
+++ src/Strings/TestSubString.java
@@ -0,0 +1,14 @@
+package Strings;
+
+
+public class TestSubString{
+    public static void main(String args[]){
+        String s="SriSindhu";
+        System.out.println(s.substring(6));
+        System.out.println(s.substring(0,6));
+        String s1="Sachin";
+        System.out.println(s1.toUpperCase());//SACHIN
+        System.out.println(s1.toLowerCase());//sachin
+        System.out.println(s1);//Sachin(no change in original)
+    }
+}
