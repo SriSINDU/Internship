561a6fe54b95285fd8ebc88df612dbf0d5c7ac01
SriSINDU <srisindu.3@gmail.com>
2019-05-17 18:13:11 +0530

    strings

--- src/ExceptionHandling/ExceptionExample.java
+++ src/ExceptionHandling/ExceptionExample.java
@@ -1,4 +1,12 @@
 package ExceptionHandling;
 
 public class ExceptionExample {
+    public static void main(String args[]){
+        try{
+            //code that may raise exception
+            int data=100/0;
+        }catch(ArithmeticException e){System.out.println(e);}
+        //rest code of the program
+        System.out.println("You cannot divide any number by Zero. Please make a note.");
+    }
 }
--- src/oopsconcepts/CheckSuper.java
+++ src/oopsconcepts/CheckSuper.java
@@ -1,13 +1,12 @@
 package oopsconcepts;
-
-public class S{
+ class S{
     private  int a=10;
     protected int b;
 }
 public class CheckSuper extends S{
     int a=20;
     void display(){
-        System.out.println(super.a);
+        System.out.println(a);
     }
    public static void main(String[] args){
         CheckSuper cs=new CheckSuper();
