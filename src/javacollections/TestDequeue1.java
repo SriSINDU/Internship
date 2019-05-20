bb522dc269dbe3f0efc37f1de8c9cf29c3ae6924
SriSINDU <srisindu.3@gmail.com>
2019-05-20 14:14:14 +0530

    Java Collection

--- src/javacollections/TestDequeue1.java
+++ src/javacollections/TestDequeue1.java
@@ -6,8 +6,18 @@ public class TestDequeue1 {
         deque.add("Gautam");
         deque.add("Karan");
         deque.add("Ajay");
+        deque.offer("arvind");
+        deque.offer("vimal");
+        deque.add("mukul");
+        deque.offerFirst("jai");
         for (String str : deque) {
             System.out.println(str);
         }
+        deque.pollLast();
+        System.out.println("After pollLast() Traversal...");
+        for(String s:deque){
+            System.out.println(s);
+        }
     }
+
 }
