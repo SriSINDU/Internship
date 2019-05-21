3eadc64d7879b69cb59dedaa876474b446e25ddc
SriSINDU <srisindu.3@gmail.com>
2019-05-21 21:57:52 +0530

    contact Application

--- src/contactapplication/ContactMain.java
+++ src/contactapplication/ContactMain.java
@@ -8,15 +8,23 @@ public class ContactMain {
 
     public static void main(String[] args){
         int ch,subch;
-        StringBuilder name,ph,mail,addr;
+        //StringBuffer sb=new StringBuffer(50);
+        String num,addr,name,email;
         Scanner s= new Scanner(System.in);
         do{
             System.out.println(" Enter any one of the options\n1.Create new Contact\n2.Update Contact\n3.Delete Contact\n4.Search Contact\n5.Exit");
             ch=s.nextInt();
             switch (ch){
                 case 1:System.out.println("Enter the contact details: \nEnter the name  ");
-
+                        name=s.nextLine();
+                        System.out.println("Enter the phone number ");
+                        num=s.nextLine();
+                        System.out.println("Enter the email");
+                        email=s.nextLine();
+                        System.out.println("Enter the address");
+                        addr=s.nextLine();
                          break;
+
                 case 2:System.out.println("on what basis you want to update enter that option!!\n 1.name 2.email 3.phone");
                         subch=s.nextInt();
                         break;
