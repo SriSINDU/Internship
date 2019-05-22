d1dd4d22b40363ca64aeca41aa0d1b216ec946f9
SriSINDU <srisindu.3@gmail.com>
2019-05-22 11:47:46 +0530

    contact Application

--- src/contactapplication/ContactMain.java
+++ src/contactapplication/ContactMain.java
@@ -11,39 +11,116 @@ public class ContactMain {
         //StringBuffer sb=new StringBuffer(50);
         String num,addr,name,email;
         Map<String, List<String>> multimap = new HashMap<String, List<String>>();
-        ArrayList<String> ar=new ArrayList<String>();
+        Map<String, List<String>> multimap1 = new HashMap<String, List<String>>();
+        Map<String, List<String>> multimap2 = new HashMap<String, List<String>>();
+
         Scanner s= new Scanner(System.in);
         do{
-            System.out.println(" Enter any one of the options\n1.Create new Contact\n2.Update Contact\n3.Delete Contact\n4.Search Contact\n5.Exit");
+            System.out.println(" Enter any one of the options\n1.Create new Contact\n2.Update Contact\n3.Delete Contact\n4.Search Contact\n5.Display Contacts\n6.Exit");
             ch=s.nextInt();
             switch (ch){
-                case 1:System.out.println("Enter the contact details: \nEnter the name  ");
-                        s.next();
-                        name=s.nextLine();
+                case 1:ArrayList<String> ar=new ArrayList<String>();
+                        ArrayList<String> ar1=new ArrayList<String>();
+                        ArrayList<String> ar2=new ArrayList<String>();
+                        System.out.println("Enter the contact details: \nEnter the name  ");
+                        s.nextLine();
+                        name=s.next();
+                        ar1.add(name);
+                        ar2.add(name);
                         System.out.println("Enter the phone number ");
+                        s.nextLine();
                         num=s.nextLine();
                         ar.add(num);
+                        ar1.add(num);
                         System.out.println("Enter the email");
                         email=s.nextLine();
                         ar.add(email);
+                        ar2.add(num);
                         System.out.println("Enter the address");
                         addr=s.nextLine();
                         ar.add(addr);
+                        ar1.add(addr);
+                        ar2.add(addr);
                         multimap.put(name,ar);
+                        multimap.put(email,ar1);
+                        multimap.put(num,ar2);
                          break;
 
                 case 2:System.out.println("on what basis you want to update enter that option!!\n 1.name 2.email 3.phone");
                         subch=s.nextInt();
+                        if(subch==1){
+                            System.out.println("enter the name");
+                            s.nextLine();
+                            name=s.next();
+                        }else if(subch==2){
+                            System.out.println("enter the email");
+                            email=s.nextLine();
+                        }else if(subch==3){
+                            System.out.println("enter the mobile number");
+                            num=s.nextLine();
+                        }
                         break;
                 case 3:System.out.println("on what basis you want to delete enter that option!!\n 1.name 2.email 3.phone");
                         subch=s.nextInt();
+                    if(subch==1){
+                        System.out.println("enter the name");
+                        s.nextLine();
+                        name=s.next();
+                        if(multimap.containsKey(name)){
+                            multimap.remove(name);
+                        }else{
+                            System.out.println("The contact doesn't exist!!!");
+                        }
+                    }else if(subch==2){
+                        System.out.println("enter the email");
+                        email=s.nextLine();
+                        if(multimap1.containsKey(email)){
+                            multimap1.remove(email);
+                        }else{
+                            System.out.println("The contact doesn't exist!!!");
+                        }
+                    }else if(subch==3){
+                        System.out.println("enter the mobile number");
+                        num=s.nextLine();
+                        if(multimap2.containsKey(num)){
+                            multimap2.remove(num);
+                        }else{
+                            System.out.println("The contact doesn't exist!!!");
+                        }
+                    }
                         break;
                 case 4:System.out.println("on what basis you want to search enter that option!!\n 1.name 2.email 3.phone");
                         subch=s.nextInt();
-                        System.out.println(multimap);
+                    if(subch==1){
+                        System.out.println("enter the name");
+                        s.nextLine();
+                        name=s.next();
+                        if(multimap.containsKey(name)){
+                            System.out.println(name+multimap.get(name));
+                        }else{
+                            System.out.println("The contact doesn't exist!!!");
+                        }
+                    }else if(subch==2){
+                        System.out.println("enter the email");
+                        email=s.nextLine();
+                        if(multimap1.containsKey(email)){
+                            System.out.println(email+multimap1.get(email));
+                        }else{
+                            System.out.println("The contact doesn't exist!!!");
+                        }
+                    }else if(subch==3){
+                        System.out.println("enter the mobile number");
+                        num=s.nextLine();
+                        if(multimap2.containsKey(num)){
+                            System.out.println(num+multimap.get(num));
+                        }else{
+                            System.out.println("The contact doesn't exist!!!");
+                        }
+                    }
                         break;
-                case 5:
+                case 5: System.out.println(multimap);
+                case 6:
             }
-        }while(ch !=5);
+        }while(ch !=6);
     }
 }
