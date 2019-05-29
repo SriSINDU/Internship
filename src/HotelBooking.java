d35b8fa88e0170c78baf5ea599d746739300805e
SriSINDU <srisindu.3@gmail.com>
2019-05-29 16:07:26 +0530

    -

--- src/HotelBooking.java
+++ src/HotelBooking.java
@@ -56,8 +56,92 @@ public class HotelBooking {
                     }
 
                      break;
-            case 3:  break;
-            case 5:  break;
+            case 3:  if(days<2){
+                        amt=days*60;
+                        amt1=days*58;
+                        if(amt<amt1 && amt<=money){
+                            System.out.println("Hotel booked for "+days+" days in "+rating+" star Hotel"+" Raddison for the cost of "+amt);
+                        }
+                        else if(amt1<amt && amt1<money){
+                            System.out.println("Hotel booked for "+days+" days in "+rating+" star Hotel"+" Rain Tree for the cost of "+amt1);
+                        }
+                        else{
+                            System.out.println("Sorry! you dont have sufficient amount to book any hotel");
+                        }
+                    }
+                    if(days<=3){
+                        dis=(18*60*days)/100;
+                        amt=(days*60)-dis;
+                        amt1=days*58;
+                        if(amt<amt1 && amt<=money){
+                            System.out.println("Hotel booked for "+days+" days in "+rating+" star Hotel"+" Raddison for the cost of "+amt);
+                        }
+                         else if(amt1<amt && amt1<money){
+                            System.out.println("Hotel booked for "+days+" days in "+rating+" star Hotel"+" Rain Tree for the cost of "+amt1);
+                        }
+                        else{
+                        System.out.println("Sorry! you dont have sufficient amount to book any hotel");
+                         }
+
+                    }
+                    if(days>3){
+                        dis=(15*58*days)/100;
+                        amt=(days*60);
+                        amt1=days*58-dis;
+                        if(amt<amt1 && amt<=money){
+                            System.out.println("Hotel booked for "+days+" days in "+rating+" star Hotel"+" Raddison for the cost of "+amt);
+                        }
+                        else if(amt1<amt && amt1<money){
+                            System.out.println("Hotel booked for "+days+" days in "+rating+" star Hotel"+" RainTree for the cost of "+amt1);
+                        }
+                        else{
+                            System.out.println("Sorry! you dont have sufficient amount to book any hotel");
+                        }
+                    }
+                     break;
+            case 5:  if(days<2){
+                    amt=days*90;
+                    amt1=days*82;
+                    if(amt<amt1 && amt<=money){
+                        System.out.println("Hotel booked for "+days+" days in "+rating+" star Hotel"+" The park for the cost of "+amt);
+                    }
+                    else if(amt1<amt && amt1<money){
+                        System.out.println("Hotel booked for "+days+" days in "+rating+" star Hotel"+" Hyat for the cost of "+amt1);
+                    }
+                    else{
+                        System.out.println("Sorry! you dont have sufficient amount to book any hotel");
+                     }
+                    }
+                    if(days<=3){
+                        dis=(20*90*days)/100;
+                        amt=(days*90)-dis;
+                        amt1=days*82;
+                        if(amt<amt1 && amt<=money){
+                            System.out.println("Hotel booked for "+days+" days in "+rating+" star Hotel"+" The Park for the cost of "+amt);
+                        }
+                        else if(amt1<amt && amt1<money){
+                        System.out.println("Hotel booked for "+days+" days in "+rating+" star Hotel"+" Hyat for the cost of "+amt1);
+                         }
+                        else{
+                        System.out.println("Sorry! you dont have sufficient amount to book any hotel");
+                        }
+
+                    }
+                    else {
+                        dis=(12*82*days)/100;
+                        amt=(days*90);
+                        amt1=days*82-dis;
+                        if(amt<amt1 && amt<=money){
+                            System.out.println("Hotel booked for "+days+" days in "+rating+" star Hotel"+" The Park for the cost of "+amt);
+                        }
+                        else if(amt1<amt && amt1<money){
+                             System.out.println("Hotel booked for "+days+" days in "+rating+" star Hotel"+" Hyat for the cost of "+amt1);
+                        }
+                        else{
+                            System.out.println("Sorry! you don't have sufficient amount to book any hotel");
+                        }
+                     }
+                    break;
         }
     }
 }
